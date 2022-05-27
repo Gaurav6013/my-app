25cc47f503a0230a1c173d0a750d8a3f369fec3e
Gaurav6013 <gauravdevganiya445@gmail.com>
2022-05-27 15:38:34 +0530

    rcc

---
+++ src/Components/Gaurav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Gaurav extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hi! finally new project</h1>
+            </div>
+        );
+    }
+}
+
+export default Gaurav;
