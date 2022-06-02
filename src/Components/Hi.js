66a77693e996ebf482f75285fd2173727a7079ab
Gaurav6013 <gauravdevganiya445@gmail.com>
2022-06-02 15:05:07 +0530

    create table

---
+++ src/Components/Hi.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hi extends Component {
+    render() {
+        return (
+            <div>
+                <p>hi!My project is Reactjs</p>
+            </div>
+        );
+    }
+}
+
+export default Hi;
